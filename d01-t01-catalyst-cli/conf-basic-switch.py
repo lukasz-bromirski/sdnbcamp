98bc1f9f809a2b5bf57cb500a60567289814c7a6
Lukasz Bromirski <lukasz.bromirski@gmail.com>
2020-06-21 19:15:08 +0200

    Basic switch automation script in Python

---
+++ d01-t01-catalyst-cli/conf-basic-switch.py
@@ -0,0 +1,40 @@
+import csv
+import re
+import sys
+from cli import configure, cli
+
+if len(sys.argv) != 3:
+    raise ValueError('Usage:\nconf-basic-switch.py input.csv device_name_to_match')
+
+input_csv_filename = sys.argv[1]
+input_device_name = sys.argv[2]
+
+tmp_name = cli("show running-config | i hostname")
+local_switch_name = tmp_name.split(" ")
+
+with open(input_csv_filename) as f:
+    reader = csv.reader(f)
+    for row in reader:
+     if( row[0] == local_switch_name[1].rstrip() ):
+      if ( row[2] == 'S' ):
+         # switchport
+         print ("Interface name: " + row[1] + " access VLAN: " + row[3])
+         print(['- interface {}'.format(row[1]), 'switchport', 'switchport mode access', 'switchport access vlan {}'.format(row[3]), 'no shutdown'])
+         configure(['interface {}'.format(row[1]), 'switchport', 'switchport mode access', 'switchport access vlan {} '.format(row[3]), 'no shutdown'])
+      elif ( row[2] == 'L' ):
+         # loopback
+         print ("Interface name: " + row[1] + " IP address: " + row[3] + " netmask " + row[4])
+         print(['- interface {}'.format(row[1]), 'ip address {} {}'.format(row[3],row [4]), 'no shutdown'])
+         configure(['interface {}'.format(row[1]), 'ip address {} {}'.format(row[3],row[4]), 'no shutdown'])
+      elif ( row[2] == 'I' ):
+         # IP interface
+         print ("Interface name: " + row[1] + " IP address: " + row[3] + " netmask " + row[4])
+         print(['- interface {}'.format(row[1]), 'no switchport', 'ip address {} {}'.format(row[3],row [4]), 'no shutdown'])
+         configure(['interface {}'.format(row[1]), 'no switchport', 'ip address {} {}'.format(row[3],row[4]), 'no shutdown'])
+      elif ( row[2] == 'T' ):
+         # trunk
+         print ("Interface name: " + row[1] + " trunk" )
+         print(['- interface {}'.format(row[1]), 'switchport', 'switchport mode trunk'])
+         configure(['interface {}'.format(row[1]), 'switchport', 'switchport mode trunk'])
+      else:
+         print ("Unrecognized port type")
